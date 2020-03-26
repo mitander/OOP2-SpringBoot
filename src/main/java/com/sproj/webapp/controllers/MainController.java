4fd94e3126c9242491f83245b774143e64d9211e
Carl Mitander <carlmitander@gmail.com>
2020-03-26 13:21:31 +0100

    added main controller

---
+++ src/main/java/com/sproj/webapp/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.seedling.webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value = { "/", "/index" })
+	public String homepage() {
+		return "index";
+	}
+
+}
